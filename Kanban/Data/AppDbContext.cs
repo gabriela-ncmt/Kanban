a25e035abc3d9b4736fe2481b9699b394a012575
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-10 20:48:25 -0300

    Creating models

--- Kanban/Data/AppDbContext.cs
+++ Kanban/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Kanban.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kanban.Data
 {
@@ -9,5 +10,19 @@ namespace Kanban.Data
             
         }
 
+        public DbSet<ActivityModel> Activities { get; set; }
+        public DbSet<ActivityModel> Status { get; set; }
+
+        //Initializing the Status table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StatusModel>().HasData(
+                new StatusModel { Id = 1, Name="Pending"},
+                new StatusModel { Id = 2, Name="OnGoing"},
+                new StatusModel { Id = 3, Name="Done"}
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Kanban/Models/ActivityModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Kanban.Models
+{
+    public class ActivityModel
+    {
+        public int Id { get; set; }
+        //matricula
+        public string Enrollment { get; set; }
+        public  string Description { get; set; }
+        public DateTime CreatedDate { get; set;} = DateTime.Now;
+        public int StatusId { get; set; }
+        public StatusModel Status { get; set; }
+
+    }
+}
