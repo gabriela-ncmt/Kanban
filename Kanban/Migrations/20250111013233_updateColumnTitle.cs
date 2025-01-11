285d4bd5e6a1186cfceae67918ca98d1b03b4c59
Gabriela Nascimento <gabrielaf.nascimento@outlook.com>
2025-01-10 23:00:55 -0300

    adding new migration to include new column Title

---
+++ Kanban/Migrations/20250111013233_updateColumnTitle.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kanban.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateColumnTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Enrollment",
+                table: "Activities",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Activities");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Enrollment",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
